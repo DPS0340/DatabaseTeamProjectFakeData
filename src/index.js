713b51fd673ba4d9fe4f0433ec40fc66837dd8cc
jiho lee <optional.int@kakao.com>
2020-11-30 15:41:23 +0900

    Change format

--- src/index.js
+++ src/index.js
@@ -6,14 +6,15 @@ const generateFunction = require('./generateFunction')
 
 
 // Main Function.
-;    (async() => {
+;
+(async() => {
     const fields = auxiliary.fields
     const funcs = generateFunction.funcs
     const result = new Map()
     const queries = []
     const creates = []
     const table = new Map()
-    for(const field of fields) {
+    for (const field of fields) {
         result[field] = []
         const head = `CREATE TABLE ${field} (`
         const tail = ");"
@@ -22,28 +23,28 @@ const generateFunction = require('./generateFunction')
         let hasPk = false
         const tables = Object.keys(res).map(tableName => {
             let annotation = ""
-            if(!hasPk && auxiliary.PK.includes(tableName)) {
+            if (!hasPk && auxiliary.PK.includes(tableName)) {
                 annotation = " NOT NULL PRIMARY KEY"
                 hasPk = true
-            } else if(auxiliary.FK.includes(tableName)) {
+            } else if (auxiliary.FK.includes(tableName)) {
                 annotation = ` FOREIGN KEY REFERENCES ${tableName.match(/[A-Z][a-z]+/g)[0].toLowerCase()}(${field})`
             }
-            let type = auxiliary.typeCheck(tableName)
+            let type = auxiliary.typeCheck(res[tableName])
             return `${tableName} ${type}${annotation}`
         }).join(',\n')
         creates.push(head + tables + tail)
-        for(let i = 0; i < 10; i++) {
+        for (let i = 0; i < 10; i++) {
             const selectedFun = funcs[fields.indexOf(field)]
             const res = await selectedFun()
             const placeholders = []
             const values = []
             table[field] = []
-            for(const [k, v] of Object.entries(res)) {
-                if(k.includes('DateTime')) {
+            for (const [k, v] of Object.entries(res)) {
+                if (k.includes('DateTime')) {
                     continue;
                 }
                 placeholders.push(k)
-                if((typeof v === 'string' || v instanceof String) && !v.includes("TO_UTC_TIMESTAMP")) {
+                if ((typeof v === 'string' || v instanceof String) && !v.includes("TO_UTC_TIMESTAMP")) {
                     values.push(`'${v}'`)
                 } else {
                     values.push(v)
@@ -56,7 +57,7 @@ const generateFunction = require('./generateFunction')
         }
     }
     const queriesString = `${creates.join('\n\n')}\n\n${queries.join('\n')}`
-    fs.writeFileSync('./sampleData/table.json', JSON.stringify(table), encoding="utf-8")
-    fs.writeFileSync('./sampleData/query.txt', queriesString, encoding="utf-8")
-    fs.writeFileSync('./sampleData/sampleDatadata.json', JSON.stringify(result), encoding="utf-8")
+    fs.writeFileSync('./sampleData/table.json', JSON.stringify(table), encoding = "utf-8")
+    fs.writeFileSync('./sampleData/query.txt', queriesString, encoding = "utf-8")
+    fs.writeFileSync('./sampleData/sampleDatadata.json', JSON.stringify(result), encoding = "utf-8")
 })()
